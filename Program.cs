47709d1d1e97c3cc3cc2f079ce75dfe71faf5af0
KristijonasNork <64493027+KristijonasNork@users.noreply.github.com>
2020-05-26 23:10:46 +0300

    sukuriamas studentas

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using System;
+
+
+namespace Labaratoriniai_csharp
+{
+    class Studentas{
+        string vardas, pavarde;
+        int[] ndRez;
+        int egzRez;
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
