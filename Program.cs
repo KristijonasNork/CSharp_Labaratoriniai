7647ee3c6f73d3a22c9cd9ab5774291f178b83e4
KristijonasNork <64493027+KristijonasNork@users.noreply.github.com>
2020-05-26 23:16:05 +0300

    Sukuriamas konstruktorius

--- Program.cs
+++ Program.cs
@@ -8,13 +8,24 @@ namespace Labaratoriniai_csharp
         int[] ndRez;
         int egzRez;
 
+        Studentas(string vardas, string pavarde, int[] ndRez, int egzRez)
+        {
+            this.vardas = vardas;
+            this.pavarde = pavarde;
+            this.ndRez = ndRez;
+            this.egzRez = egzRez;
+
+        }
     }
+
+
 
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
 
             Console.ReadLine();
         }
