b69025f2ea24eb332739ecd80ead75b3618d4177
KristijonasNork <64493027+KristijonasNork@users.noreply.github.com>
2020-05-27 23:34:49 +0300

    Prideti 2 try/catch'ai

--- Program.cs
+++ Program.cs
@@ -62,10 +62,18 @@ namespace Labaratoriniai_csharp
                         Console.WriteLine("parasykite studento balus, kai baigsite parasykite -1: ");
                         while (balas != -1)
                         {
-                            balas = Convert.ToDouble(Console.ReadLine());
-                            if (balas != -1)
+                            try
                             {
-                                balai.Add(balas);
+                                balas = Convert.ToDouble(Console.ReadLine());
+                                if (balas != -1)
+                                {
+                                    balai.Add(balas);
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine(ex);
+                                Console.WriteLine("blogai parasytas balas");
                             }
                         }
                     }
@@ -123,19 +131,28 @@ namespace Labaratoriniai_csharp
                             string line = sr.ReadLine();
                             while (!string.IsNullOrEmpty((line = sr.ReadLine())))
                             {
-                                // Vardas Pavardė ND1 ND2 ND3 ND4 ND5 Egzaminas
-                                var reiksmes = line.Split(" ");
-                                var vardas = reiksmes[0];
-                                var pavarde = reiksmes[1];
-                                Console.WriteLine(reiksmes);
-                                double[] namuDarbai = new double[5] { Convert.ToDouble(reiksmes[2]) , Convert.ToDouble(reiksmes[3]), Convert.ToDouble(reiksmes[4]), Convert.ToDouble(reiksmes[5]), Convert.ToDouble(reiksmes[6]) };
-                                double egzBalas = Convert.ToDouble(reiksmes[7]);
-                                studentai.Add(new Studentas(vardas, pavarde, namuDarbai, egzBalas));
-                                // C:\Users\Kristijonas\source\repos\Labaratoriniai_csharp\Labaratoriniai_csharp\Studentai.txt
-                                Console.WriteLine("pridetasstudentas " + vardas + " " + pavarde);
+                                try
+                                {
+                                    // Vardas Pavardė ND1 ND2 ND3 ND4 ND5 Egzaminas
+                                    var reiksmes = line.Split(" ");
+                                    var vardas = reiksmes[0];
+                                    var pavarde = reiksmes[1];
+                                    Console.WriteLine(reiksmes);
+                                    double[] namuDarbai = new double[5] { Convert.ToDouble(reiksmes[2]), Convert.ToDouble(reiksmes[3]), Convert.ToDouble(reiksmes[4]), Convert.ToDouble(reiksmes[5]), Convert.ToDouble(reiksmes[6]) };
+                                    double egzBalas = Convert.ToDouble(reiksmes[7]);
+                                    studentai.Add(new Studentas(vardas, pavarde, namuDarbai, egzBalas));
+                                    // C:\Users\Kristijonas\source\repos\Labaratoriniai_csharp\Labaratoriniai_csharp\Studentai.txt
+                                    Console.WriteLine("pridetasstudentas " + vardas + " " + pavarde);
+                                }
+                                catch(Exception e)
+                                {
+                                    Console.WriteLine(e);
+                                    Console.WriteLine("blogai suformatuotas failas");
+                                    break;
+                                }
+                                Console.WriteLine("Failas egzistuoja ir yra ikeltas.");
                             }
                         }
-                        Console.WriteLine("Failas egzistuoja ir yra ikeltas.");
                     }
                     else
                     {
